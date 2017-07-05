7e1b1f8f8143313f84336e5d080160c6d5ea6a71
Ivy Chenyao <ivychenyao@ivys-air.coldspring.marian.edu>
2017-07-05 14:34:12 -0400

    Add Sidebar, NoteList, NoteForm components

---
+++ src/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './Sidebar.css'
+
+// This IS the render function, no need for separate
+const Sidebar = () => {
+    return <p>SIDEBAR!</p>
+}
+
+export default Sidebar
