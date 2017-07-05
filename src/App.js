c8b5b2a1e3126158af085a10ec118b1521f2ac4d
Ivy Chenyao <ivychenyao@ivys-air.coldspring.marian.edu>
2017-07-05 16:12:08 -0400

    Store hard-coded notes in App's state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import './App.css'
 import Main from './Main'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      notes = {
+        'note-4': {
+          id: 'note-4',
+          title: 'Note from obj',
+          body: 'Oh so fancy, I do declare!',
+        },
+        'note-5': {
+          id: 'note-5',
+          title: 'Note from another obj',
+          body: 'Also very fancy',
+        },
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Main />
+        <Main notes={this.state.notes} />
       </div>
     )
   }
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,27 +3,16 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = () => {
-    const note = {
-        'note-4': {
-            id: 'note-4',
-            title: 'Note from obj',
-            body: 'Oh so fancy, I do declare!',
-        },
-        'note-5': {
-            id: 'note-5',
-            title: 'Note from another obj',
-            body: 'Also very fancy',
-        },
-    }
-
-    const noteIds = Object.keys(notes) // ['note-4, 'note-5']
+const NoteList = (props) => {
+    const noteIds = Object.keys(props.notes)
 
     return (
         <div className="NoteList">
             <h3>Notes</h3>
             <ul id="notes">
-                {noteIds.map(noteId => <Note note = {notes[noteId]} />)}
+                {noteIds.map(noteId => (
+                    <Note note = {props.notes[noteId]} />
+                ))}
             </ul>
         </div>
     )
