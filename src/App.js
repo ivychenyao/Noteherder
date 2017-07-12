4b7838e58e6717eaf12f33e8254635fda004ce96
Ivy Chenyao <ivychenyao@ivys-air.coldspring.marian.edu>
2017-07-12 14:27:20 -0400

    Add a default route in App

--- src/App.js
+++ src/App.js
@@ -143,6 +143,11 @@ class App extends Component {
                 : <Redirect to="/sign-in" />
             )} 
           />
+          <Route render={() => (
+            this.signedIn()
+              ? <Redirect to="/notes" />
+              : <Redirect to="/sign-in" />
+          )} />
         </Switch>
       </div>
     )
